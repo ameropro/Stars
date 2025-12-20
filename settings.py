708f87d7739020e87ef5f239310ef3c08d9b541b
ameropro <73493546+ameropro@users.noreply.github.com>
2025-12-20 15:25:56 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,49 @@
+TOKEN = "8359470401:AAF-jzY2hmFfurK_d6Fu2qUEFKQmB22-TPM" #сюда токен бота
+
+admins_id = [
+    5885013209,
+]
+
+SUBGRAM_TOKEN = "" # токен subgram
+FLYER_API = "FL-mKmjsG-LHElIB-qtFdKa-hgLYvR"
+
+admin_username = "@ameropro" #свой username
+admin_link = "https://t.me/ameropro"
+
+nac_1 = [3.5] # 1 Уровень награды за реферала
+nac_2 = [4] # 2 Уровень награды за реферала
+nac_3 = [4.5] # 3 Уровень награды за реферала
+
+refs_boost = [0] # Количество рефералов в неделю для вывода (с бустом)
+refs_noboost = [0] # Количество рефералов в неделю для вывода (без буста)
+
+task_grant = [0.5] # Награда за задание
+
+click_grant = [0.1] # Награда за клик
+
+boost_cost = [599] # Стоимость буста
+
+button_subgram = [False] 
+
+GIFT_AMOUNT = [0.5] # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 600 # Задержка на кликер (в секундах)
+
+#основной канал
+channel_osn = "https://t.me/amerostars"
+#чат
+chater = "https://t.me/amerostars_chat"
+id_chat = -1003464731716
+#канал выплат
+channel_viplat = "https://t.me/amerostars_otz"
+channel_viplat_id = -1003367817301
+#канал выигрышей мини-игры:
+channel_link = "https://t.me/amerostars_game"
+id_channel_game = -1003613444207
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
